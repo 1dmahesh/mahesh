ddbb30c0d385deb0a0ef29e7f7479ce7532b837d
1dmahesh <maheshj469@gmail.com>
2020-08-13 13:17:59 +0000

    new3

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3500;
 
-app.get("/", (req, res) => res.send("Hello from 1dmahesh joshi!"));
+app.get("/", (req, res) => res.send("Hello from 1dmahesh!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
